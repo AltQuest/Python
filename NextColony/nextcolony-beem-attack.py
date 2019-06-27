c23d4a9a3a5a486eca338dffe7490d06408ee83e
AltQuest <altquest09@gmail.com>
2019-06-27 18:53:55 -0400

    JSON fix
    changed JSON
    added a sleep and loop to autoattack :p

--- NextColony/nextcolony-beem-attack.py
+++ NextColony/nextcolony-beem-attack.py
@@ -7,15 +7,29 @@ nodes = ['https://steemd.minnowsupportproject.org/']
 PK = 'posting key here'
 accountname =  'bitcoinjake09'
 myPlanet = "P-ZU1H748F2TS"
-shipName = "corvette"
+shipName = "corvette1"
 shipCount = 2
+sleepAmt = (60 * 60 * 6) #set total time for ships to and back to loop right :p
+count = 0
 # Address of person I don't like
 x = 241
 y = -41
+while(count <= 999):
+	s = Steem(node = nodes, keys=[PK])
+	id = 'nextcolony'
+	json_data = {
+  "username": accountname,
+  "type": "attack",
+  "command": {
+    "tr_var1": { shipName: { "pos": 1, "n": shipCount } },
+    "tr_var2": x,
+    "tr_var3": y,
+    "tr_var4": myPlanet
+  }
+}
 
-s = Steem(node = nodes, keys=[PK])
-id = 'nextcolony'
-json_data = {'username': accountname, "type": "attack", "command": {"tr_var1":{shipName:{"Pos":1, "N":shipCount}},"tr_var2":x,"tr_var3": y, "tr_var4":myPlanet}}
-
-s.custom_json(id, json_data, required_posting_auths=[accountname])
-print json_data
+	print("will sleep now for: ", sleepAmt, " seconds")
+	s.custom_json(id, json_data, required_posting_auths=[accountname])
+	print json_data
+	count+1
+	time.sleep(sleepAmt)
