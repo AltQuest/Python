0bd54541c79d0c416cdfa44ce48fe2353cb6224d
AltQuest <altquest09@gmail.com>
2019-06-29 21:31:24 -0400

    updated timing loop, added display progress
    updated timing loop, added display progress

--- NextColony/nextcolony-beem-attack.py
+++ NextColony/nextcolony-beem-attack.py
@@ -6,15 +6,18 @@ nodes = ['https://steemd.minnowsupportproject.org/']
 
 PK = 'posting key here'
 accountname =  'bitcoinjake09'
-myPlanet = "P-ZU1H748F2TS"
-shipName = "corvette1"
-shipCount = 2
-sleepAmt = (60 * 60 * 6) #set total time for ships to and back to loop right :p
-count = 0
-# Address of person I don't like
-x = 241
-y = -41
-while(count <= 999):
+myPlanet = "P-Z6AY2BGWNIO"
+shipName = "corvette"
+shipCount = 3
+sleepAmt = ((3244) * 2) #set as (time it takes ships to get to planet + 4 seconds for steem blockchain) *2 for total mission
+# can use a seconds calculator if you know your ships time from spycolony distance calculator:
+# https://spycolony.herokuapp.com/calc.html
+# seconds calculator: https://www.dollartimes.com/calculators/hours-minutes-calculator.htm
+count = 1
+# Address of person I don't like x/y
+x = -24
+y = 249
+while(count <= 1001):
 	s = Steem(node = nodes, keys=[PK])
 	id = 'nextcolony'
 	json_data = {
@@ -31,5 +34,9 @@ while(count <= 999):
 	print("will sleep now for: ", sleepAmt, " seconds")
 	s.custom_json(id, json_data, required_posting_auths=[accountname])
 	print json_data
-	count+1
-	time.sleep(sleepAmt)
+	progressTime=0
+	while(progressTime<=sleepAmt):
+		print(progressTime,"/",sleepAmt, " until next attack #", (count+1))
+		time.sleep(1)
+		progressTime = progressTime + 1
+	count = count + 1
